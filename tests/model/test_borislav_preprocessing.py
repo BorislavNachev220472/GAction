bf9c3ffb8ba0f174c49c046401d7e9fb8d8a540d
Borislav Nachev <220472@buas.nl>
2024-05-13 23:32:24 +0200

    unit test workflow

--- tests/model/test_borislav_preprocessing.py
+++ tests/model/test_borislav_preprocessing.py
@@ -4,7 +4,7 @@ from src.model import borislav_preprocessing as bp
 
 class TestGetYearsToPredict(object):
 
-    def test_get_years_to_predict_should_return_correct_value(self, mocker):
+    def test_get_years_to_predict_should_return_correct_value(self):
         actual = bp.demo()
         expected = True
         assert actual == expected
