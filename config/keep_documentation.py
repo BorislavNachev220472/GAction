35732e7b6463fc0dd089b7be2202c09e358103f9
Borislav Nachev <220472@buas.nl>
2024-05-14 08:50:31 +0200

    unit test workflow

--- config/keep_documentation.py
+++ config/keep_documentation.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 working_directory = r"./"
-retain = ["config", ".github", ".git", "src"]
+retain = ["config", ".github", "src"]
 os.chdir(working_directory)
 
 for item in next(os.walk(working_directory))[1]:
